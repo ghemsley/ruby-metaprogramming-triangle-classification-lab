98bcb4a71b543859eac19b60195de1146968e056
ghemsley <ghemsley@protonmail.ch>
2020-11-20 18:12:38 -0600

    commit tree 2d1733d9f8accc7f72c85c551270540a67fbaa70

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    self.side1 = side1
+    self.side2 = side2
+    self.side3 = side3
+  end
+
+  def kind
+    raise TriangleError if [side1, side2, side3].include?(0) || [side1, side2, side3].any?(&:negative?)
+    raise TriangleError if side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1
+    return :equilateral if side1 == side2 && side2 == side3
+    return :isosceles if side1 == side2 || side2 == side3 || side1 == side3
+
+    :scalene
+  end
+
+  class TriangleError < StandardError; end
 end
+
+triangle = Triangle.new(2, 2, 2)
+puts triangle.kind
